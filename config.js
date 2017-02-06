726ebc071e043ccd04306c8ed7fdf633461edb0c
Akin909 <akin.sowemimo@gmail.com>
2017-02-06 19:42:12 +0000

    completed twitter bot looking for nodejs hashtag posting to my account

---
+++ config.js
@@ -0,0 +1,19 @@
+//Config.js
+/** TWITTER APP CONFIGURATION
+	* consumer_key
+FAbEy6vnDgYqAXL5wpmdR4M0c	
+	* consumer_secret
+    ZPeD9ht9OrXvgfYWoEcACUuMBCDZtBTrkfx760WTZgCogNT7UK	
+	* access_token
+	2397194284-A65roEolvDgELu7s8LgrlTLDJVz6a9mqcwTAxiR	
+	* access_token_secret
+dK4NwFsnAi9FApaBXK4hh47ICiKkmQtFJvCmOEMWwHJ07
+	*/
+
+module.exports = {
+consumer_key: 'FAbEy6vnDgYqAXL5wpmdR4M0c',
+consumer_secret: 'ZPeD9ht9OrXvgfYWoEcACUuMBCDZtBTrkfx760WTZgCogNT7UK',
+access_token: '2397194284-A65roEolvDgELu7s8LgrlTLDJVz6a9mqcwTAxiR',
+access_token_secret: 'dK4NwFsnAi9FApaBXK4hh47ICiKkmQtFJvCmOEMWwHJ07'
+
+}

4f0b12849a2029415822434429d2d9a353f6b134
Akin909 <akin.sowemimo@gmail.com>
2017-02-07 07:56:46 +0000

    add heroku environment variables and gitignore

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ dK4NwFsnAi9FApaBXK4hh47ICiKkmQtFJvCmOEMWwHJ07
 	*/
 
 module.exports = {
-consumer_key: 'FAbEy6vnDgYqAXL5wpmdR4M0c',
-consumer_secret: 'ZPeD9ht9OrXvgfYWoEcACUuMBCDZtBTrkfx760WTZgCogNT7UK',
-access_token: '2397194284-A65roEolvDgELu7s8LgrlTLDJVz6a9mqcwTAxiR',
-access_token_secret: 'dK4NwFsnAi9FApaBXK4hh47ICiKkmQtFJvCmOEMWwHJ07'
+consumer_key:process.env.consumer_key,
+consumer_secret:process.env.consumer_key,
+access_token:process.env.access_token,
+access_token_secret: process.env.access_token_secret
 
 }
